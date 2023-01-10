999f36d8b91bd8d2cc541f5294ef32c0c909c944
israpps <isr_apps_help@hotmail.com>
2023-01-10 11:39:27 -0300

    tweak
    [skip actions]

--- external/PS2-SIOCookie/include/SIOCookie.h
+++ external/PS2-SIOCookie/include/SIOCookie.h
@@ -7,6 +7,6 @@
 
 int ee_sio_start(u32 baudrate, u8 lcr_ueps, u8 lcr_upen, u8 lcr_usbl, u8 lcr_umode, int vbuftype);
 
-enum {EESIO_SUCESS = 0, EESIO_COOKIE_OPEN_IS_NULL, EESIO_BUFFER_ALLOC_IS_NULL};
+enum EE_SIO_START_RETCODES{EESIO_SUCESS = 0, EESIO_COOKIE_OPEN_IS_NULL, EESIO_BUFFER_ALLOC_IS_NULL};
 extern FILE *EE_SIO;
 #endif
