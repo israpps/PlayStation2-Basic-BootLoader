e448abca09179d14917899f38cd01fb8cf7b928b
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-05-22 11:22:09 -0300

    reset xparams on startup

--- src/main.c
+++ src/main.c
@@ -215,7 +215,8 @@ int main(int argc, char *argv[])
             break;
         }
     } while ((STAT & 0x80) || (result == 0));
-
+    DPRINTF("Resetting DECKARD XPARAMS\n");
+    ResetDeckardXParams();
     // Remember to set the video output option (RGB or Y Cb/Pb Cr/Pr) accordingly, before SetGsCrt() is called.
     DPRINTF("Setting vmode\n");
     SetGsVParam(OSDConfigGetVideoOutput() == VIDEO_OUTPUT_RGB ? VIDEO_OUTPUT_RGB : VIDEO_OUTPUT_COMPONENT);
