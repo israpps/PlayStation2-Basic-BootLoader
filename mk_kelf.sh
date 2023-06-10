318c44a43f1e48b0c2de2bef24daacf2ff7fdcf3
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-06-10 12:20:53 -0300

    add HDD build into `mk_kelf.sh`

--- mk_kelf.sh
+++ mk_kelf.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+rm -f bin/SYSTEM.XLF bin/XSYSTEM.XLF bin/HSYSTEM.XLF
 echo compiling PS2 KELF
-make clean kelf
-mv bin/PS2BBL.KELF bin/SYSTEM.XLF
-make clean
+make clean kelf --silent
+mv bin/PS2BBL_MC.KELF bin/SYSTEM.XLF
+make clean --silent
 echo compiling PSX KELF
-make clean kelf PSX=1
-mv bin/PSXBBL.KELF bin/XSYSTEM.XLF
+make clean kelf PSX=1 --silent
+mv bin/PSXBBL_MC.KELF bin/XSYSTEM.XLF
+make clean PSX=1 --silent
+
+echo compiling PS2 HDD KELF
+make clean kelf HDD=1 --silent
+mv bin/PS2BBL_HDD.KELF bin/HSYSTEM.XLF
+make clean HDD=1 --silent
 
 echo "done!"
