adba377e1b7673e162a41395cf3735d9e510e1f3
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-06-22 00:54:04 -0300

    mk_kelf: pass argumments to all `make`'s

--- mk_kelf.sh
+++ mk_kelf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 rm -f bin/SYSTEM.XLF bin/XSYSTEM.XLF bin/HSYSTEM.XLF
 echo compiling PS2 KELF
-make clean kelf --silent
+make clean kelf $* --silent
 mv bin/PS2BBL_MC.KELF bin/SYSTEM.XLF
-make clean --silent
+make clean $* --silent
 echo compiling PSX KELF
-make clean kelf PSX=1 --silent
+make clean kelf PSX=1 $* --silent
 mv bin/PSXBBL_MC.KELF bin/XSYSTEM.XLF
-make clean PSX=1 --silent
+make clean PSX=1 $* --silent
 
 echo compiling PS2 HDD KELF
-make clean kelf HDD=1 --silent
+make clean kelf HDD=1 $* --silent
 mv bin/PS2BBL_HDD.KELF bin/HSYSTEM.XLF
-make clean HDD=1 --silent
+make clean HDD=1 $* --silent
 
 echo "done!"
