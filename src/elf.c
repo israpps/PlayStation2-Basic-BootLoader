1e0cbfb89c115e1d6e57b13aab12e48f8500d878
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-07-13 00:29:24 -0300

    revert cf8f802 & add wait time before loading ELF
    wait time only applied if PS2BBL has debugging enabled
    
    fixes #21

--- src/elf.c
+++ src/elf.c
@@ -3,15 +3,30 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <tamtypes.h>
-#include <sifrpc.h>
 #include <kernel.h>
 #include <elf-loader.h>
 #include "debugprintf.h"
 #define MAX_PATH 1025
+#ifdef DEBUG
+#define DBGWAIT(T) sleep(T)
+#else
+#define DBGWAIT(T)
+#endif
 
 
 void RunLoaderElf(const char *filename, const char *party)
 {
-	DPRINTF("LoadELFFromFileWithPartition(%s, %s, 0, NULL);\n", filename, party);
-    LoadELFFromFileWithPartition(filename, party, 0, NULL);
+    DPRINTF("%s\n", __FUNCTION__);
+    if (party == NULL)
+	{
+		DPRINTF("LoadELFFromFile(%s, 0, NULL)\n", filename);
+        DBGWAIT(2);
+        LoadELFFromFile(filename, 0, NULL);
+	}
+    else
+	{
+		DPRINTF("LoadELFFromFileWithPartition(%s, %s, 0, NULL);\n", filename, party);
+        DBGWAIT(2);
+        LoadELFFromFileWithPartition(filename, party, 0, NULL);
+	}
 }
