6241dbfc85c8392b3f5a95bfeb33923ff4781665
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-08-09 10:07:32 -0300

    add intellisense to ioplib
    should this become an IRX Library to be proposed on PS2SDK?

--- iop/sd2psxman/ioplib.h
+++ iop/sd2psxman/ioplib.h
@@ -17,10 +17,22 @@
 
 #include <loadcore.h>
 
-
+/** @brief get library object of any IRX Module
+ * @param name name of the module to search for
+ * @return NULL on error.
+ */
 iop_library_t *ioplib_getByName(const char *name);
 unsigned int ioplib_getTableSize(iop_library_t *lib);
+
+
+/** @brief hook an IRX Module exported function
+ * @param lib Library object of the module to hook
+ * @param entry Export number to be hooked
+ * @param func Hook function
+ * @return on error: NULL | on success: original function pointer
+ */
 void *ioplib_hookExportEntry(iop_library_t *lib, unsigned int entry, void *func);
+
 void ioplib_relinkExports(iop_library_t *lib);
 
 
